0bf4601b7bab7563449bf8d0bdf008b4d008c29f
Mothmannn <lprupp10@gmail.com>
2026-03-25 19:39:57 -0400

    Reoraganized project structure. Docker midsetup. Added marshmallow.

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,16 +1,24 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
 from .config import Config
 
 
 db = SQLAlchemy()
+ma=Marshmallow()
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
     
     db.init_app(app)
+    ma.init_app(app)
     
+    from .routes import main
     from . import models
+    app.register_blueprint(main)
+
+    with app.app_context():
+        db.create_all()
     
     return app
