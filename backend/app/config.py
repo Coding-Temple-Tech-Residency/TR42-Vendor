cfa62d39e7d83be937d1ce794ec66f303e3df810
Taylor Putman <taylorputman41@gmail.com>
2026-03-29 13:27:01 -0600

    Refactor config and run files; update requirements for Flask and dependencies

--- backend/app/config.py
+++ backend/app/config.py
@@ -4,6 +4,6 @@ import os
 # load_dotenv()
 
 class Config:
-    os.getenv("SECRET_KEY", "dev")
+    SECRET_KEY = os.getenv("SECRET_KEY", "dev")
     SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
--- backend/run.py
+++ backend/run.py
@@ -1,5 +1,5 @@
 from app import create_app
-from server.app.extensions import db
+from app.extensions import db
 
 app = create_app()
 
