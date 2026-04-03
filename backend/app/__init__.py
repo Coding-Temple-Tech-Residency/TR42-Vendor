82a5c5c1dabaea8c280709fb62ebc76f1af6e466
arshaprajesh <arshababu333@gmail.com>
2026-04-02 22:02:07 -0400

    added comments

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -2,6 +2,9 @@ from flask import Flask
 from app.extensions import db, ma
 from app.config import Config
 from app.logging_config import setup_logging
+# from work_order import register_work_order
+# from ticket import register_ticket
+# from contractor import register_contractor
 
 
 def create_app():
@@ -23,6 +26,42 @@ def create_app():
         from app.blueprints.vendor_users.model import VendorUser
         from app.blueprints.address.model import Address
         from app.blueprints.vendor.model import Vendor
+
+        # try:
+    #     from app.blueprints.vendor_users import register_vendor_user
+
+    #     app.register_blueprint(register_vendor_user, url_prefix="/vendor_user")
+    # except ImportError:
+    #     pass
+
+    # try:
+    #     from app.blueprints.vendors import register_vendors
+
+    #     app.register_blueprint(register_vendors)
+    # except ImportError as e:
+    #     print(f"Vendor blueprint import failed: {e}")
+
+    # try:
+    #     from app.blueprints.work_order import register_work_order
+
+    #     app.register_blueprint(register_work_order)
+    # except ImportError as e:
+    #     print(f"Work order blueprint import failed: {e}")
+
+    # try:
+    #     from app.blueprints.ticket import register_ticket
+
+    #     app.register_blueprint(register_ticket)
+    # except ImportError as e:
+    #     print(f"Ticket blueprint import failed: {e}")
+
+    # try:
+    #     from app.blueprints.contractor import register_contractor
+
+    #     app.register_blueprint(register_contractor)
+    # except ImportError as e:
+    #     print(f"Contractor blueprint import failed: {e}")
+
 
         # Create tables AFTER all models are imported
         db.create_all()
