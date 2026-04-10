5ea1e5321edef16636e6094cda03e23776775530
arshaprajesh <arshababu333@gmail.com>
2026-04-10 14:21:17 -0400

    added service module

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -55,7 +55,8 @@ def create_app(config_object=None):
     from app.blueprints.work_orders.controller.work_order_routes import work_order_bp
     from app.blueprints.invoices.controller.invoice_routes import invoice_bp
     from app.blueprints.invoices.controller.lineitem_routes import lineItem_bp
-
+    from app.blueprints.services.controller.service_routes import services_bp
+    
     app.register_blueprint(user_bp, url_prefix="/api/users")
     app.register_blueprint(address_bp, url_prefix="/api/addresses")
     app.register_blueprint(vendor_bp, url_prefix="/api/vendors")
@@ -64,5 +65,6 @@ def create_app(config_object=None):
     app.register_blueprint(work_order_bp, url_prefix="/api/work_orders")
     app.register_blueprint(invoice_bp, url_prefix="/api/invoices")
     app.register_blueprint(lineItem_bp, url_prefix="/api/line_items")
+    app.register_blueprint(services_bp, url_prefix="/services")
 
     return app
--- backend/app/models.py
+++ backend/app/models.py
@@ -4,4 +4,6 @@ from app.blueprints.vendor_user.model import VendorUser
 from app.blueprints.address.model import Address
 from app.blueprints.work_orders.model import WorkOrder
 from app.blueprints.well.model import Well
+
 from app.blueprints.invoices.model import Invoice
+from app.blueprints.services.model import Service
